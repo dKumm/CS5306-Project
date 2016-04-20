4929f894b209fc98d2aeb5d1d1cec983d8de6cf7
dKumm <dk569@cornell.edu>
2016-04-20 13:30:49 -0400

    Create questions.py
    Hard-coded questions

---
+++ questions.py
@@ -0,0 +1,54 @@
+questions = [
+
+	('Which one of the five is least like the other four?',
+	['Dog',  'Mouse', 'Lion', 'Snake', 'Elephant'], 3),
+
+	('Which number should come next in the series 1 - 1 - 2 - 3 - 5 - 8 - 13 ?',
+	['8', '13', '21', '26', '31'], 2),
+
+	('Which one of the five choices makes the best comparison? PEACH is to HCAEP as 46251 is to:',
+	['25641', '26451', '12654', '51462', '15264'], 4),
+
+	('Mary, who is sixteen years old, is four times as old as her brother. How old will Mary be when she is twice as old as her brother?',
+	['20', '24', '25', '26', '28'], 1),
+
+	('Which one of the numbers does not belong in the following series? 2 - 3 - 6 - 7 - 8 - 14 - 15 - 30',
+	['Three', 'Seven', 'Eight', 'Fifteen', 'Thirty'], 2),
+
+	('Which one of the five choices makes the best comparison? Finger is to Hand as Leaf is to:',
+	['Twig', 'Tree', 'Branch', 'Blossom', 'Bark'], 0),
+
+	('If you rearrange the letters "CIFAIPC" you would have the name of a(n):',
+	['City', 'Animal', 'Ocean', 'River', 'Country'], 2),
+
+	('Choose the number that is 1/4 of 1/2 of 1/5 of 200:',
+	['2', '5', '10', '25', '50'], 1),
+
+	('John needs 13 bottles of water from the store. John can only carry 3 at a time. What\'s the minimum number of trips John needs to make to the store?',
+	['3', '4', ' 4.5', '5', '6'], 3),
+	
+	('If all Bloops are Razzies and all Razzies are Lazzies, all Bloops are definitely Lazzies?',
+	['True', 'False'], 0),
+
+	('Choose the word most similar to "Trustworthy":',
+	['Resolute', 'Tenacity', 'Relevant', 'Insolent', 'Reliable'], 4),
+
+	('If you rearrange the letters "LNGEDNA" you have the name of a(n):',
+	['Animal', 'Country', 'State', 'City', 'Ocean'], 1),
+
+	('Which one of the numbers does not belong in the following series? 1 - 2 - 5 - 10 - 13 - 26 - 29 - 48',
+	['1', '5', '26', '29', '48'], 4),
+
+	('Ralph likes 25 but not 24; he likes 400 but not 300; he likes 144 but not 145. Which does he like:',
+	['10', '50', '124', '200', '1600'], 4),
+
+	('What is the missing number in the sequence shown below? 1 - 8 - 27 - ? - 125 - 216',
+	['36', '45', '46', '64', '99'], 3),
+
+	('Which one of the following things is the least like the others?',
+	['Poem', 'Novel', 'Painting', 'Statue', 'Flower'], 4)
+
+]
+
+for question in questions:c
+	print question[1][question[2]]
